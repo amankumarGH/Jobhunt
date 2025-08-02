708272f7713440ee2b8a44483d6401c9ec54a1eb
amankumarGH <amankumaredi@gmail.com>
2025-08-03 00:02:47 +0530

    adding the login and signup pages route

--- client/src/pages/Navbar.jsx
+++ client/src/pages/Navbar.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div>
-      
+    <div className="p-7 w-[90%] flex mx-auto">
+      <div className="flex justify-between w-full items-center">
+        <Link to="/" className="text-4xl font-bold ">
+          JobPortal
+        </Link>
+        <div className="flex items-center gap-10">
+          <ul className="flex gap-7 text-lg font-bold">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link>Job</Link>
+            </li>
+            <li>
+              <Link>Browser</Link>
+            </li>
+          </ul>
+          <div className="flex gap-3">
+            <Link to="/login">
+              <Button className="p-5">Login</Button>
+            </Link>
+            <Link to="/signup">
+              <Button className="p-5">SignUp</Button>
+            </Link>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
