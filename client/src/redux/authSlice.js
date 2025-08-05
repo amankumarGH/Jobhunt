2c8c5dd5aed3af4aae21e3a9db9920ae8cfb54ed
amankumarGH <amankumaredi@gmail.com>
2025-08-05 11:56:39 +0530

    add the local storage

--- client/src/redux/authSlice.js
+++ client/src/redux/authSlice.js
@@ -1,15 +1,26 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+const initialAuthUser = localStorage.getItem("authUser")
+  ? JSON.parse(localStorage.getItem("authUser"))
+  : null;
+
 const authSlice = createSlice({
   name: "auth",
-  initialState: { loading: false },
+  initialState: {
+    loading: false,
+    authUser: initialAuthUser,
+  },
   reducers: {
     setLoading: (state, action) => {
       state.loading = action.payload;
     },
+    setAuthUser: (state, action) => {
+      state.authUser = action.payload;
+    },
+    
   },
 });
 
-export const { setLoading } = authSlice.actions;
+export const { setLoading, setAuthUser } = authSlice.actions;
 
 export default authSlice.reducer;
